44a0cad20af93cd2332eab331c2fd41d5d2bc59a
Kusal Fernando <kusal@sigscale.org>
2019-02-14 18:13:56 +0530

    change type to @type

--- src/im_rest_res_resource.erl
+++ src/im_rest_res_resource.erl
@@ -229,8 +229,8 @@ resource([category | T], #{"category" := Category} = M, Acc)
 	resource(T, M, Acc#resource{category = Category});
 resource([class_type | T], #resource{class_type = Type} = R, Acc)
 		when is_list(Type) ->
-	resource(T, R, Acc#{"type" => Type});
-resource([class_type | T], #{"type" := Type} = M, Acc)
+	resource(T, R, Acc#{"@type" => Type});
+resource([class_type | T], #{"@type" := Type} = M, Acc)
 		when is_list(Type) ->
 	resource(T, M, Acc#resource{class_type = Type});
 resource([base_type | T], #resource{base_type = Type} = R, Acc)
