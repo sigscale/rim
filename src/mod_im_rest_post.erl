ed7cf7243513a934465e2ab3cd3516f22658071f
Kusal Fernando <kusal@sigscale.org>
2019-02-21 10:51:40 +0530

    use correct method with correct number of arguments

--- src/mod_im_rest_post.erl
+++ src/mod_im_rest_post.erl
@@ -94,8 +94,8 @@ do_post(Resource, ModData, Body, ["resourceCatalogManagement", "v3", "resourceCa
 	do_response(ModData, Resource:post_candidate(Body));
 do_post(Resource, ModData, Body, ["resourceCatalogManagement", "v3", "resourceSpecification"]) ->
 	do_response(ModData, Resource:post_specification(Body));
-do_post(Resource, ModData, Body, ["resourceInventoryManagement", "v3", "resource", ID]) ->
-	do_response(ModData, Resource:add_resource(ID, Body)).
+do_post(Resource, ModData, Body, ["resourceInventoryManagement", "v3", "resource"]) ->
+	do_response(ModData, Resource:post_resource(Body)).
 
 %% @hidden
 do_response(#mod{data = Data} = ModData, {ok, Headers, ResponseBody}) ->
