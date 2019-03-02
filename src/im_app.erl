68f0d0a43d755ebfc4abe51e7c188fd50a14bb77
Vance Shipley <vances@sigscale.org>
2019-03-02 22:22:32 +0530

    add 5g nr resource specifications to table

--- src/im_app.erl
+++ src/im_app.erl
@@ -270,6 +270,8 @@ install8(Nodes, Acc) ->
 		umts_rnc, umts_nodeb, umts_cell_fdd,
 		umts_cell_tdd_lcr, umts_cell_tdd_hcr,
 		lte_enb, lte_cell_fdd, lte_cell_tdd,
+		nr_gnb_du, nr_gnb_cu_cp, nr_gnb_cu_up,
+		nr_cell_cu, nr_cell_du,
 		epc_sgw, epc_pgw, epc_mme, epc_pcrf,
 		core_msc, core_mgw, core_sgsn, core_ggsn,
 		ims_as, ims_hss, ims_pcscf, ims_scscf, ims_icscf],
