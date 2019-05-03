f95b3148261bcedc33e5056294cd3bf4d74fddb4
Vance Shipley <vances@sigscale.org>
2019-05-03 13:17:23 +0000

    appease dialyzer

--- src/im_xml.hrl
+++ src/im_xml.hrl
@@ -13,63 +13,63 @@
 		{subnet = [] :: [string()],
 		me_context = [] :: [string()],
 		managed_element = [] :: [string()],
-		vs_data :: map(),
+		vs_data :: map() | undefined,
 		vs_data_container = [] :: [map()]}).
 -type generic_state() :: #generic_state{}.
 
 -record(geran_state,
-		{bss :: map(),
-		bts :: map(),
-		cell :: map(),
-		gsm_rel :: map(),
+		{bss :: map() | undefined,
+		bts :: map() | undefined,
+		cell :: map() | undefined,
+		gsm_rel :: map() | undefined,
 		btss = [] :: [string()],
 		cells = [] :: [string()]}).
 -type geran_state() :: #geran_state{}.
 
 -record(utran_state,
-		{rnc :: map(),
-		fdd :: map(),
-		utran_rel :: map(),
-		tdd_lcr :: map(),
-		tdd_hcr :: map(),
-		nodeb :: map(),
-		iub :: map(),
-		iucs :: map(),
-		iups :: map(),
-		iur :: map(),
+		{rnc :: map() | undefined,
+		fdd :: map() | undefined,
+		utran_rel :: map() | undefined,
+		tdd_lcr :: map() | undefined,
+		tdd_hcr :: map() | undefined,
+		nodeb :: map() | undefined,
+		iub :: map() | undefined,
+		iucs :: map() | undefined,
+		iups :: map() | undefined,
+		iur :: map() | undefined,
 		fdds = [] :: [string()]}).
 -type utran_state() :: #utran_state{}.
 
 -record(epc_state,
-		{epdg :: map(),
-		mme :: map(),
-		pcrf :: map(),
-		pgw :: map(),
-		sgw :: map(),
-		ep_rp_eps :: map(),
-		ep_rp_epss :: [string()]}).
+		{epdg :: map() | undefined,
+		mme :: map() | undefined,
+		pcrf :: map() | undefined,
+		pgw :: map() | undefined,
+		sgw :: map() | undefined,
+		ep_rp_eps :: map() | undefined,
+		ep_rp_epss = [] :: [string()]}).
 -type epc_state() :: #epc_state{}.
 
 -record(core_state,
-		{msc :: map(),
-		iucs :: map(),
-		mgw :: map(),
-		ggsn :: map(),
-		sgsn :: map(),
-		iups :: map()}).
+		{msc :: map() | undefined,
+		iucs :: map() | undefined,
+		mgw :: map() | undefined,
+		ggsn :: map() | undefined,
+		sgsn :: map() | undefined,
+		iups :: map() | undefined}).
 -type core_state() :: #core_state{}.
 
 -record(zte_state,
-		{bts :: map(),
-		vs_data :: map(),
-		cell :: map(),
+		{bts :: map() | undefined,
+		vs_data :: map() | undefined,
+		cell :: map() | undefined,
 		cells = [] :: [string()],
 		vs_data_container = [] :: [map()]}).
 -type zte_state() :: #zte_state{}.
 
 -record(huawei_state,
-		{gsm_function :: map(),
-		umts_function :: map(),
-		bts :: map(),
-		nodeb :: map()}).
+		{gsm_function :: map() | undefined,
+		umts_function :: map() | undefined,
+		bts :: map() | undefined,
+		nodeb :: map() | undefined}).
 -type huawei_state() :: #huawei_state{}.
