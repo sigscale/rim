7743c1f4ad4e077ca359be2afc11b6bc16f9c6a2
Vance Shipley <vances@sigscale.org>
2019-05-10 15:23:56 +0000

    typo

--- src/mod_im_rest_get.erl
+++ src/mod_im_rest_get.erl
@@ -131,7 +131,7 @@ do_get(Resource, ModData,
 	do_response(ModData, Resource:get_specification(Id, Query));
 do_get(Resource, #mod{parsed_header = Headers, method = Method} = ModData,
 		["resourceInventoryManagement", "v3", "resource"], Query) ->
-   do_response(ModData, Resource:get_resources(method = Method, Query, Headers));
+   do_response(ModData, Resource:get_resources(Method, Query, Headers));
 do_get(Resource, #mod{parsed_header = _Headers} = ModData,
 		["resourceInventoryManagement", "v3", "resource", Id], Query) ->
    do_response(ModData, Resource:get_resource(Id, Query));
