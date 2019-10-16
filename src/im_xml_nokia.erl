a349c8b11caa1369168df2b7c61190f0d8b51211
Kusal Fernando <kusal@sigscale.org>
2019-10-16 07:22:28 +0000

    add resource for ManagedElement for gsm and lte in nokia

--- src/im_xml_nokia.erl
+++ src/im_xml_nokia.erl
@@ -23,7 +23,8 @@
 -export([import/2, parse_mo/2, parse_bss/2, parse_bts/2, parse_gsm_cell/2,
 		parse_hw/2,
 		parse_rnc/2, parse_nodeb/2, parse_iub_link/2,
-		parse_enb/2]).
+		parse_enb/2,
+		parse_me/2]).
 
 -include("im.hrl").
 -include_lib("inets/include/mod_auth.hrl").
@@ -106,6 +107,12 @@ parse_xml(_Event, _Location, [#state{parse_module = Mod, parse_function = F} | _
 
 %% @hidden
 parse_mo({startElement, _, "managedObject", QName,
+		[{[], [], "class", "BSC"}, _, {[], [], "distName", DN}, _] = Attributes},
+		[#state{dn_prefix = [], stack = Stack, rule = RuleId} | _] = State) ->
+		[#state{parse_module = ?MODULE, parse_function = parse_me,
+		dn_prefix = [DN], rule = RuleId,
+		stack = [{startElement, QName,Attributes} | Stack]} | State];
+parse_mo({startElement, _, "managedObject", QName,
 		[{[], [], "class", "BCF"}, _, {[], [], "distName", DN}, _] = Attributes},
 		[#state{dn_prefix = [], stack = Stack, rule = RuleId} | _] = State) ->
 		[#state{parse_module = ?MODULE, parse_function = parse_bss,
@@ -148,6 +155,12 @@ parse_mo({startElement, _, "managedObject", QName,
 		dn_prefix = [DN], rule = RuleId,
 		stack = [{startElement, QName, Attributes} | Stack]} | State];
 parse_mo({startElement, _, "managedObject", QName,
+		[{[], [], "class", "MRBTS"}, _, {[], [], "distName", DN}, _] = Attributes},
+		[#state{dn_prefix = [], stack = Stack, rule = RuleId} | _] = State) ->
+		[#state{parse_module = ?MODULE, parse_function = parse_me,
+		dn_prefix = [DN], rule = RuleId,
+		stack = [{startElement, QName,Attributes} | Stack]} | State];
+parse_mo({startElement, _, "managedObject", QName,
 		[{[], [], "class", "LNBTS"}, _, {[], [], "distName", DN}, _] = Attributes},
 		[#state{dn_prefix = [], stack = Stack, rule = RuleId} | _] = State) ->
 		[#state{parse_module = ?MODULE, parse_function = parse_enb,
@@ -156,6 +169,72 @@ parse_mo({startElement, _, "managedObject", QName,
 parse_mo(_Event, [#state{parse_module = ?MODULE,
 		parse_function = parse_mo} | _] = State) ->
 	State.
+
+%% @hidden
+parse_me({characters, SideId}, [#state{rule = RuleId,
+		stack = [{startElement, {_, "p"}, [{[], [], "name", "name"}]} | _]} = State | T]) ->
+	case im:get_pee(RuleId, SideId) of
+		{ok, []} ->
+			[State | T];
+		{ok, PEEMonitoredEntities} ->
+			PeeParametersList =
+					parse_peeParameterslist(PEEMonitoredEntities, []),
+			[State#state{location = PeeParametersList} | T];
+		{error, _Reason} ->
+			[State | T]
+	end;
+parse_me({characters, Chars}, [#state{stack = Stack} = State | T]) ->
+	[State#state{stack = [{characters, Chars} | Stack]} | T];
+parse_me({startElement, _, _, QName, Attributes},
+		[#state{stack = Stack} = State | T]) ->
+	[State#state{stack = [{startElement, QName, Attributes} | Stack]} | T];
+parse_me({endElement, _Uri, "managedObject", QName},
+		[#state{dn_prefix = [MeDn | _], stack = Stack, location = Location,
+		spec_cache = Cache}, #state{spec_cache = PrevCache} = PrevState | T1]) ->
+	{[_ | T2], _NewStack} = pop(startElement, QName, Stack),
+	MeAttr = parse_me_attr(T2, undefined, []),
+	ClassType = "ManagedElement",
+	{Spec, NewCache} = get_specification_ref(ClassType, Cache),
+	PeeParam = #resource_char{name = "peeParametersList",
+			class_type = "PeeParametersListType", value = Location,
+			schema = "/resourceCatalogManagement/v3/schema/genericNrm#/"
+					"definitions/PeeParametersListType"},
+	Resource = #resource{name = MeDn,
+			description = "",
+			category = "",
+			class_type = ClassType,
+			base_type = "ResourceFunction",
+			schema = "/resourceInventoryManagement/v3/schema/ManagedElement",
+			specification = Spec,
+			characteristic = [PeeParam | MeAttr]},
+	case im:add_resource(Resource) of
+		{ok, #resource{} = _R} ->
+			[PrevState#state{spec_cache = [NewCache | PrevCache]} | T1];
+		{error, Reason} ->
+			throw({add_resource, Reason})
+	end;
+parse_me({endElement, _Uri, _LocalName, QName} = _Event,
+		[#state{stack = Stack} = State | T]) ->
+	[State#state{stack = [{endElement, QName} | Stack]} | T].
+
+% @hidden
+parse_me_attr([{startElement, {[], "p"},
+		[{[], [], "name", Attr}]} | T], undefined, Acc) ->
+	parse_me_attr(T, Attr, Acc);
+parse_me_attr([{startElement, {[], "defaults"},
+		[{[], [], "name", Attr}]} | T], undefined, Acc) ->
+	parse_me_attr(T, Attr, Acc);
+parse_me_attr([{startElement, {_, "list"} = QName, _} | T1], undefined, Acc) ->
+	% @todo bscOptions
+	{[_ | _BscOptions], T2} = pop(endElement, QName, T1),
+	parse_me_attr(T2, undefined, Acc);
+parse_me_attr([{characters, Chars} | T], Attr, Acc) ->
+	parse_me_attr(T, Attr,
+			[#resource_char{name = Attr, value = Chars} | Acc]);
+parse_me_attr([{endElement, {[], _}} | T], _Attr, Acc) ->
+	parse_me_attr(T, undefined, Acc);
+parse_me_attr([], undefined, Acc) ->
+	Acc.
 
 %% @hidden
 parse_bss({characters, SideId}, [#state{rule = RuleId,
