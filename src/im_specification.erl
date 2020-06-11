660ab22788baff96cbdfd21341a693f6a79b7f49
Kusal Fernando <kusal@sigscale.org>
2020-06-11 09:31:57 +0000

    add specification for 5gc EP_N16 connection point

--- src/im_specification.erl
+++ src/im_specification.erl
@@ -35,8 +35,8 @@
 		ngc_n3iwf/0, ngc_pcf/0, ngc_ausf/0, ngc_udm/0, ngc_udr/0, ngc_udsf/0,
 		ngc_nrf/0, ngc_nssf/0, ngc_smsf/0, ngc_lmf/0, ngc_ngeir/0, ngc_sepp/0,
 		ngc_nwdaf/0, ngc_ep_n2/0, ngc_ep_n4/0, ngc_ep_n7/0, ngc_ep_n8/0,
-		ngc_ep_n10/0, ngc_ep_n11/0,
-		ngc_ep_n12/0, ngc_ep_n14/0, ngc_ep_n15/0, ngc_ep_n17/0, ngc_ep_n20/0,
+		ngc_ep_n10/0, ngc_ep_n11/0, ngc_ep_n12/0, ngc_ep_n14/0, ngc_ep_n15/0,
+		ngc_ep_n16/0, ngc_ep_n17/0, ngc_ep_n20/0,
 		ngc_ep_n22/0, ngc_ep_n26/0, ngc_ep_nls/0, ngc_ep_nlg/0, ngc_ep_sbi_x/0]).
 -export([epc_sgw/0, epc_pgw/0, epc_mme/0, epc_pcrf/0, epc_epdg/0,
 		epc_link_mme_mme/0, epc_link_hss_mme/0, epc_link_mme_sgsn/0,
@@ -3115,6 +3115,40 @@ ngc_ep_n15() ->
 			category = "5GC",
 			target_schema = #target_schema_ref{class_type = "EP_N15",
 					schema = "/resourceInventoryManagement/v3/schema/EP_N15"},
+			characteristic = Chars}.
+
+-spec ngc_ep_n16() -> specification().
+%% @doc 5GC End Point N16 resource specification.
+ngc_ep_n16() ->
+	ID = #specification_char{name = "id",
+			description = "Used as an RDN when naming an instance of the object class.",
+			value_type = "string"},
+	UserLabel = #specification_char{name = "userLabel",
+			description = "A user-friendly (and user assignable) name of this object",
+			value_type = "string"},
+	FarEndEntity = #specification_char{name = "farEndEntity",
+			description = [],
+			value_type = "string"},
+	LocalAddress = #specification_char{name = "localAddress",
+			description = "Includes IP address and VLAN ID used for initialization"
+					"of the underlying transport",
+			value_type = "string"},
+	RemoteAddress = #specification_char{name = "remoteAddress",
+			description = "IP address used for initialization of the"
+					"underlying transport",
+			value_type = "string"},
+	Chars = [ID, UserLabel, FarEndEntity, LocalAddress, RemoteAddress],
+	#specification{name = "EP_N16",
+			description = "5G Core End Point of N16 interface (between two SMFs)"
+					"resource specification",
+			class_type = "EP_N16Spec",
+			schema = "/resourceCatalogManagement/v3/schema/EP_N16Spec",
+			base_type = "ResourceFunctionSpecification",
+			status = active,
+			version = "1.0",
+			category = "Core",
+			target_schema = #target_schema_ref{class_type = "EP_N16",
+					schema = "/resourceInventoryManagement/v3/schema/EP_N16"},
 			characteristic = Chars}.
 
 -spec ngc_ep_n17() -> specification().
