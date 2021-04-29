2cf4f1e62f8464e47ee1fc975eae5a2b5cb4a456
Vance Shipley <vances@sigscale.org>
2021-04-30 02:18:42 +0800

    include scroll effects for waterfall

--- priv/www/src/inventory-management.js
+++ priv/www/src/inventory-management.js
@@ -14,6 +14,7 @@ import '@polymer/app-layout/app-drawer/app-drawer.js';
 import '@polymer/app-layout/app-drawer-layout/app-drawer-layout.js';
 import '@polymer/app-layout/app-header/app-header.js';
 import '@polymer/app-layout/app-header-layout/app-header-layout.js';
+import '@polymer/app-layout/app-scroll-effects/app-scroll-effects.js';
 import '@polymer/app-layout/app-toolbar/app-toolbar.js';
 import '@polymer/paper-progress/paper-progress.js';
 import '@polymer/app-route/app-location.js';
