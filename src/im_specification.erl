4d60c50c1569eff5e8b755680603f79bcb9b0191
Vance Shipley <vances@sigscale.org>
2021-05-06 19:10:49 +0800

    correct category for epc aaa

--- src/im_specification.erl
+++ src/im_specification.erl
@@ -5697,7 +5697,7 @@ epcn3ai_proxy() ->
 			class_type = "ResourceFunctionSpecification",
 			status = active,
 			version = "1.0",
-			category = "EPCN3AI",
+			category = "EPC",
 			target_schema = #target_schema_ref{class_type = "3GPPAAAProxyFunction",
 					schema = ?PathCatalogSchema ++ "3GPPAAAProxyFunction"},
 			characteristic = Chars}.
@@ -5725,7 +5725,7 @@ epcn3ai_server() ->
 			class_type = "ResourceFunctionSpecification",
 			status = active,
 			version = "1.0",
-			category = "EPCN3AI",
+			category = "EPC",
 			target_schema = #target_schema_ref{class_type = "3GPPAAAServerFunction",
 					schema = ?PathCatalogSchema ++ "3GPPAAAServerFunction"},
 			characteristic = Chars}.
